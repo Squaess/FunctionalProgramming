ba35a0bb5436d2f2c210deaad401e5758444568e
silentnauscopy <babartosz.BNS@gmail.com>
2020-01-29 02:51:30 +0100

    Fxing issue with long array producing

--- list4/main.hs
+++ list4/main.hs
@@ -1,4 +1,4 @@
-
+import Data.Set
 import Control.Monad
 -- Task1
 
@@ -10,10 +10,10 @@ move m p
 -- (Just 0) >>= move 1 >>= move (-2) >>= move 12
 
 move_list :: [Int] -> Int -> Maybe Int
-move_list xs pos = foldr (\a b -> b >>= move a) (Just pos) xs
+move_list xs pos = Prelude.foldr (\a b -> b >>= move a) (Just pos) xs
 
 move_list' :: [Int] -> Int -> Maybe Int
-move_list' xs pos = foldl (\b a -> b >>= move a) (Just pos) xs
+move_list' xs pos = Prelude.foldl (\b a -> b >>= move a) (Just pos) xs
 
 
 -- Task2
@@ -27,14 +27,14 @@ roll = do
 roll' :: [(Integer, Integer)]
 roll' = [1..20] >>= (\x -> [1..6] >>= (\y -> [(x,y)]))
 
---  [(x,y) | x <- [1..20], y <- [1..6]]
+roll'' = [(x,y) | x <- [1..20], y <- [1..6]]
 
 -- Task 3
 
 type KnightPos = (Int, Int)
 
 moveKnight :: Int -> Int -> KnightPos -> [KnightPos]
-moveKnight nc nr (c,r) = filter (\x -> fst x `elem` [1..nc] &&  snd x `elem` [1..nr])
+moveKnight nc nr (c,r) = Prelude.filter (\x -> fst x `elem` [1..nc] &&  snd x `elem` [1..nr])
     [
         (c+2, r+1), (c+2, r-1),
         (c-2, r+1), (c-2, r-1),
@@ -46,7 +46,8 @@ moveKnight3 :: KnightPos -> Int -> Int -> [KnightPos]
 moveKnight3 pos cn rn = return pos >>= moveKnight cn rn >>= moveKnight cn rn >>= moveKnight cn rn
 
 moveKnightN :: Int -> KnightPos -> [KnightPos]
-moveKnightN n pos = foldl (\x _ -> x >>= moveKnight 8 8) (return pos) [1..n]
+moveKnightN n pos = Prelude.foldl (\x _ -> clear (x >>= moveKnight 8 8)) (return pos) [1..n] where
+                clear = toList . fromList
 
 
 moveKnightN':: Int -> KnightPos -> [KnightPos]
